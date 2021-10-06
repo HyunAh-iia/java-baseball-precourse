edfc84acdea13e72c76a589804e4063be1183da6
iia <kha2464@gmail.com>
2021-10-07 00:34:25 +0900

    test: 사용자이 입력값 조합 검증

--- src/test/java/baseball/domain/BallsFactoryTest.java
+++ src/test/java/baseball/domain/BallsFactoryTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BallsFactoryTest {
 	@DisplayName("정답 생성")
@@ -21,5 +23,31 @@ public class BallsFactoryTest {
 		//then
 		assertThat(answer.size()).isEqualTo(defaultLength);
 		assertThat(new HashSet<>(answer).size()).isEqualTo(defaultLength);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"1", "012", "112", "67890"})
+	@DisplayName("사용자의 올바르지 않은 입력값 검증")
+	public void testValidateWrongBalls(String input) {
+		assertThatIllegalArgumentException().isThrownBy(() ->
+			BallsFactory.convertBalls(input)
+		);
+	}
+
+	@DisplayName("사용자의 올바른 입력값 검증")
+	@Test
+	public void testValidateBalls() {
+		//given
+		String validatedValue = "123";
+		int defaultLength = 3;
+
+		//when
+		List<Ball> balls = BallsFactory.convertBalls(validatedValue);
+
+		//then
+		assertThat(balls.size()).isEqualTo(defaultLength);
+		for (int i = 0; i < defaultLength; i++) {
+			assertThat(balls.get(i).getNumber()).isEqualTo(Character.getNumericValue(validatedValue.charAt(i)));
+		}
 	}
 }
